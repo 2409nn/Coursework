9c3ed2b2ddc5b77c8d481198cb5a8e37ba80c2c6
Iskanderious <A.iskander07@gmail.com>
2025-04-21 13:00:24 +0500

    работа идет 5

---
+++ functional/dropContextMenuN.js
@@ -0,0 +1,31 @@
+import { dropContextMenu, hideContextMenu } from "../functional/contextMenu.js"
+
+$(document).ready(function () {
+
+    $(".notifications .notif").on("contextmenu", function (event) {
+
+        let thisElem = $(this)
+        event.preventDefault()
+
+        $("#contextMenu").css("transform", `translate(${event.pageX}px, ${event.pageY}px)`)
+
+        dropContextMenu("Remove")
+
+        $(".removeBtn").on("click", function () {
+
+            thisElem.remove()
+            hideContextMenu($("#contextMenu"))
+
+        })
+
+        $("body").click(function (event) {
+
+            if (!event.target.closest("#contextMenu")) {
+                hideContextMenu($("#contextMenu"))
+
+            }
+
+        })
+
+    })
+})
---
+++ functional/notifsMenu.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+$(".notifs__btn").click(function(){
+
+    $(".notifications").toggleClass("active")
+})
+
+})
