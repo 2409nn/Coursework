2bff1d65fa4399ce7d4213f97ef5f2165bbf5039
Iskanderious <A.iskander07@gmail.com>
2025-05-10 20:03:44 +0500

    контекст меню 4

--- docs/functional/page.js
+++ docs/functional/page.js
@@ -31,102 +31,119 @@ $(document).ready(function () {
     })
 
     // контекстное меню (с кнопками "изменить" и "удалить")
-    $(document).on("contextmenu", "#tasks .task, #projects .goal, #todayTasks .task, #projects .project", function (event) {
+    // $(document).on("contextmenu", "#tasks .task, #projects .goal, #todayTasks .task, #projects .project", function (event) {
+    //
+    //     event.preventDefault()
+    //     dropContextMenu("Change", "Remove")
+    //     $("#contextMenu").css("transform", `translate(${event.pageX}px, ${event.pageY}px)`)
+    //
+    //     let thisElem = $(this)
+    //     let elemClassName = thisElem.attr("class")
+    //
+    //     $(".changeBtn").click(function () {
+    //         changeElem(thisElem)
+    //         hideContextMenu($("#contextMenu"))
+    //     })
+    //
+    //     $(".removeBtn").click(function () {
+    //
+    //         let list = thisElem.closest("ul")
+    //
+    //         if (elemClassName === "project") {
+    //             if (confirm("Are you sure you want to delete this project?")) {
+    //                 thisElem.remove()
+    //             }
+    //         } else {
+    //             thisElem.remove()
+    //         }
+    //         hideContextMenu($("#contextMenu"))
+    //
+    //         let weekGoalsList = $("#projects .weekGoals")
+    //         let monthGoalsList = $("#projects .monthGoals")
+    //
+    //         if (monthGoalsList.children("li").not(":has(h5)").length === 0 && weekGoalsList.children("li").not(":has(h5)").length === 0) {
+    //             $(".goals").css("display", "none")
+    //
+    //             monthGoalsList.parent(".goals").siblings(".empty__state").css("display", "flex")
+    //         }
+    //
+    //
+    //         addEmptyState(list, list.siblings(".empty__state"))
+    //     })
+    //
+    //     $("body").click(function (event) {
+    //         if (!event.target.closest("#contextMenu")) {
+    //             hideContextMenu($("#contextMenu"))
+    //         }
+    //     })
+    // })
 
-        event.preventDefault()
-        dropContextMenu("Change", "Remove")
-        $("#contextMenu").css("transform", `translate(${event.pageX}px, ${event.pageY}px)`)
+    // для IOS
+    const selectors = "#tasks .task, #projects .goal, #todayTasks .task, #projects .project";
 
-        let thisElem = $(this)
-        let elemClassName = thisElem.attr("class")
+    // 🔹 Обработчик contextmenu для Android / десктопов
+    $(document).on("contextmenu", selectors, function (event) {
+        event.preventDefault();
+        openCustomContextMenu($(this), event.pageX, event.pageY);
+    });
 
-        $(".changeBtn").click(function () {
-            changeElem(thisElem)
-            hideContextMenu($("#contextMenu"))
-        })
+    // 🔹 Hammer.js: эмуляция longpress для iOS
+    $(selectors).each(function () {
+        const el = this;
+        const hammer = new Hammer(el);
+        hammer.get('press').set({ time: 600 });
 
-        $(".removeBtn").click(function () {
+        hammer.on("press", function (ev) {
+            const touch = ev.srcEvent;
+            openCustomContextMenu($(el), touch.pageX, touch.pageY);
+        });
+    });
 
-            let list = thisElem.closest("ul")
+    // 🔹 Функция открытия контекстного меню
+    function openCustomContextMenu($elem, x, y) {
+        const elemClassName = $elem.attr("class");
+
+        dropContextMenu("Change", "Remove");
+        $("#contextMenu").css("transform", `translate(${x}px, ${y}px)`);
+
+        $(".changeBtn").off("click").on("click", function () {
+            changeElem($elem);
+            hideContextMenu($("#contextMenu"));
+        });
+
+        $(".removeBtn").off("click").on("click", function () {
+            let $list = $elem.closest("ul");
 
             if (elemClassName === "project") {
                 if (confirm("Are you sure you want to delete this project?")) {
-                    thisElem.remove()
+                    $elem.remove();
                 }
             } else {
-                thisElem.remove()
-            }
-            hideContextMenu($("#contextMenu"))
-
-            let weekGoalsList = $("#projects .weekGoals")
-            let monthGoalsList = $("#projects .monthGoals")
-
-            if (monthGoalsList.children("li").not(":has(h5)").length === 0 && weekGoalsList.children("li").not(":has(h5)").length === 0) {
-                $(".goals").css("display", "none")
-
-                monthGoalsList.parent(".goals").siblings(".empty__state").css("display", "flex")
+                $elem.remove();
             }
 
+            hideContextMenu($("#contextMenu"));
 
-            addEmptyState(list, list.siblings(".empty__state"))
-        })
+            let $weekGoalsList = $("#projects .weekGoals");
+            let $monthGoalsList = $("#projects .monthGoals");
 
-        $("body").click(function (event) {
-            if (!event.target.closest("#contextMenu")) {
-                hideContextMenu($("#contextMenu"))
+            if (
+                $monthGoalsList.children("li").not(":has(h5)").length === 0 &&
+                $weekGoalsList.children("li").not(":has(h5)").length === 0
+            ) {
+                $(".goals").css("display", "none");
+                $monthGoalsList.parent(".goals").siblings(".empty__state").css("display", "flex");
             }
-        })
-    })
 
-    // для IOS
-    $("#tasks .task, #projects .goal, #todayTasks .task, #projects .project").each(function () {
+            addEmptyState($list, $list.siblings(".empty__state"));
+        });
 
-        // если пользователь нажал и держит пол секунды, вызвать контекст меню
-        const el = this
-        const hammer = new Hammer(el)
-        hammer.get('press').set({ time: 400 })
-
-        hammer.on("press", function () {
-
-            let thisElem = $(this)
-            let elemClassName = thisElem.attr("class")
-
-            $(".changeBtn").on("click", function () {
-                changeElem(thisElem);
+        $("body").off("click.contextmenu").on("click.contextmenu", function (e) {
+            if (!$(e.target).closest("#contextMenu").length) {
                 hideContextMenu($("#contextMenu"));
-            });
-
-            $(".removeBtn").on("click", function () {
-                let list = thisElem.closest("ul");
-
-                if (elemClassName === "project") {
-                    if (confirm("Are you sure you want to delete this project?")) {
-                        thisElem.remove();
-                    }
-                } else {
-                    thisElem.remove();
-                }
-
-                hideContextMenu($("#contextMenu"));
-
-                let weekGoalsList = $("#projects .weekGoals")
-                let monthGoalsList = $("#projects .monthGoals")
-
-                if (monthGoalsList.children("li").not(":has(h5)").length === 0 && weekGoalsList.children("li").not(":has(h5)").length === 0) {
-                    $(".goals").css("display", "none")
-
-                    monthGoalsList.parent(".goals").siblings(".empty__state").css("display", "flex")
-                }
-                addEmptyState(list, list.siblings(".empty__state"))
-            })
-
-            $("body").click(function (event) {
-                if (!event.target.closest("#contextMenu")) {
-                    hideContextMenu($("#contextMenu"))
-                }
-            })
-        })
-    })
+            }
+        });
+    }
 
     $(".new__project").click(function () {
         createElem("project")
