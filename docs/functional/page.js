8eb4e2ecc61612ff666e8f17d9daa9006d7b2011
Iskanderious <A.iskander07@gmail.com>
2025-05-10 17:18:03 +0500

    контекст меню 2

--- docs/functional/page.js
+++ docs/functional/page.js
@@ -78,6 +78,63 @@ $(document).ready(function () {
         })
     })
 
+    // для IOS
+    const elements = document.querySelectorAll("#tasks .task, #projects .goal, #todayTasks .task, #projects .project");
+
+    elements.forEach(el => {
+        const hammertime = new Hammer(el);
+
+        // Настройка события press (удержание ~500ms)
+        hammertime.get('press').set({ time: 600 });
+
+        hammertime.on('press', function (event) {
+            const jEl = $(el);
+            const elemClassName = jEl.attr("class");
+
+            dropContextMenu("Change", "Remove");
+
+            // Координаты касания
+            const touch = event.srcEvent;
+            $("#contextMenu").css("transform", `translate(${touch.pageX}px, ${touch.pageY}px)`);
+
+            $(".changeBtn").off("click").on("click", function () {
+                changeElem(jEl);
+                hideContextMenu($("#contextMenu"));
+            });
+
+            $(".removeBtn").off("click").on("click", function () {
+                let list = jEl.closest("ul");
+
+                if (elemClassName === "project") {
+                    if (confirm("Are you sure you want to delete this project?")) {
+                        jEl.remove();
+                    }
+                } else {
+                    jEl.remove();
+                }
+
+                hideContextMenu($("#contextMenu"));
+
+                let weekGoalsList = $("#projects .weekGoals");
+                let monthGoalsList = $("#projects .monthGoals");
+
+                if (monthGoalsList.children("li").not(":has(h5)").length === 0 &&
+                    weekGoalsList.children("li").not(":has(h5)").length === 0) {
+                    $(".goals").css("display", "none");
+                    monthGoalsList.parent(".goals").siblings(".empty__state").css("display", "flex");
+                }
+
+                addEmptyState(list, list.siblings(".empty__state"));
+            });
+
+            $("body").off("click.contextmenu").on("click.contextmenu", function (e) {
+                if (!e.target.closest("#contextMenu")) {
+                    hideContextMenu($("#contextMenu"));
+                }
+            })
+        })
+    })
+
     $(".new__project").click(function () {
         createElem("project")
     })
